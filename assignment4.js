94248b3a380b5f11b3b36139095e7190092cfe45
pclark4422 <pclark4422@gmail.com>
2016-11-23 16:06:54 -0500

    Assignment Complete
    Got the "interests", "programming", and "all" data to display properly
    and put the data into a table for presentation.

--- assignment4.js
+++ assignment4.js
@@ -2,19 +2,71 @@
 
 (function() {
   // Magic!
-  console.log('Keepin\'n it clean with an external script!');
-
-  var $url    = $('.flexsearch-input') || '';
+  var $url= $('.flexsearch-input') || '';
 
   $('#mainForm').on('submit', function(e) {
     e.preventDefault();
+
+    $url= $('.flexsearch-input').val();
+    $('#data').html("<tr></tr>")
+
     $.ajax({
       url: $url,
       method: 'GET'
     }).success(function(data) {
-      console.log(data);
+      dataWrite(data);
     }).fail(function(data) {
-      console.log(data);
+      $('#data').html("<p>Failed to Retrieve Data</p>")
     });
   });
 })();
+
+function dataWrite(data){
+  var testType = typeof data.data[0];
+  if(testType != "string"){
+    all(data);
+  }
+  else{
+    some(data);
+  }
+}
+
+function all(data){
+  var interests = data.data.interests;
+  var programming = data.data.programming;
+
+  var maxLength = interests.length;
+
+  if(maxLength < programming.length){
+    maxLength = programming.length;
+  }
+
+  $('#data tr:last').after("<tr><th>Interests: </th> <th> Programming: </th></tr>");
+
+  for(i=0;i<maxLength;i++){
+    if(typeof interests[i] != "undefined"){
+      var interest = interests[i];
+    }
+    else{
+      var interest = ""
+    }
+
+    if(typeof programming[i] != "undefined"){
+      var program = programming[i];
+    }
+    else{
+      var program = ""
+    }
+
+      $('#data tr:last').after("<tr><td>"+ interest + "</td> <td>" + program + "</td></tr>");
+
+  }
+}
+
+function some(data){
+  var list = data.data;
+
+  for(i=0;i<list.length;i++){
+    $('#data tr:last').after("<tr><td>"+ list[i] + "</td> </tr>");
+  }
+}
